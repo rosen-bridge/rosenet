3b772c7f69afc3b115677ca863d22f896af46965
Mohammad Kermani <mkermani144@gmail.com>
2023-11-19 12:25:44 +0000

    feat: initialize relay service and add configs

---
+++ services/relay/src/configs/index.ts
@@ -0,0 +1,81 @@
+import nodeConfig from 'config';
+import { ValidationError, array, number, object, string } from 'yup';
+
+import './extend-yup';
+
+import {
+  DEFAULT_LOG_LEVEL,
+  DEFAULT_PEER_ID_FILE_PATH,
+  DEFAULT_PORT,
+} from '../constants';
+
+import { LogConfig } from '../types';
+
+/**
+ * get all service configs or exit the process in case of invalid configs
+ */
+const getAllConfigs = () => {
+  try {
+    const allowedPeers = array(
+      string()
+        .multiaddr('allowedPeers')
+        .required('allowedPeers should not contain a null or undefined value')
+        .label('allowedPeers')
+    )
+      .required()
+      .min(1)
+      .label('allowedPeers')
+      .validateSync(nodeConfig.get<string[]>('allowedPeers'));
+
+    const logs = array(
+      object({
+        type: string()
+          .oneOf(['file', 'console'])
+          .required()
+          .label('logs[index].type'),
+        path: string().path('logs[index].path').label('logs[index].path'),
+        maxSize: string().label('logs[index].maxSize'),
+        maxFiles: string().label('logs[index].maxFiles'),
+        level: string()
+          .required()
+          .oneOf(['debug', 'info', 'warn', 'error'])
+          .default(DEFAULT_LOG_LEVEL)
+          .label('logs[index].level'),
+      })
+    )
+      .label('logs')
+      .validateSync(nodeConfig.get<LogConfig[]>('logs'));
+
+    const peerIdFilePath = string()
+      .required()
+      .path('peerIdFilePath')
+      .label('peerIdFilePath')
+      .validateSync(
+        nodeConfig.get<string>('peerIdFilePath') ?? DEFAULT_PEER_ID_FILE_PATH
+      );
+
+    const port = number()
+      .required()
+      .port()
+      .label('port')
+      .validateSync(nodeConfig.get<number>('port') ?? DEFAULT_PORT);
+
+    return {
+      allowedPeers,
+      logs,
+      peerIdFilePath,
+      port,
+    };
+  } catch (error) {
+    if (error instanceof ValidationError) {
+      console.error(`ConfigError: ${error.message}`);
+    } else {
+      console.error(error);
+    }
+    process.exit(1);
+  }
+};
+
+const config = getAllConfigs();
+
+export default config;
---
+++ services/relay/src/constants.ts
@@ -0,0 +1,3 @@
+export const DEFAULT_LOG_LEVEL = 'info';
+export const DEFAULT_PEER_ID_FILE_PATH = './relay.json';
+export const DEFAULT_PORT = 10000;
