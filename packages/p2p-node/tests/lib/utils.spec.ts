b294c90345a7f68d4266135bd737a5293ed1eff6
Mohammad Kermani <mkermani144@gmail.com>
2023-11-23 16:26:57 +0330

    test(p2p-node): wrap util tests in describe statements

--- packages/p2p-node/tests/lib/utils.spec.ts
+++ packages/p2p-node/tests/lib/utils.spec.ts
@@ -1,39 +1,43 @@
-import { it, expect } from 'vitest';
+import { describe, expect, it } from 'vitest';
 
 import { objectToUint8Array, uint8ArrayToObject } from '../../lib/utils';
 
 import { testData } from './utilsTestData';
 
-/**
- * @target
- * objectToUint8Array should convert object to Uint8Array correctly
- *
- * @dependencies
- *
- * @scenario
- * - call the function under test
- * @expected
- * - converted value should be the expected one
- */
-it('should convert object to Uint8Array correctly', () => {
-  const converted = objectToUint8Array(testData.object);
+describe('objectToUint8Array', () => {
+  /**
+   * @target
+   * objectToUint8Array should convert object to Uint8Array correctly
+   *
+   * @dependencies
+   *
+   * @scenario
+   * - call the function under test
+   * @expected
+   * - converted value should be the expected one
+   */
+  it('should convert object to Uint8Array correctly', () => {
+    const converted = objectToUint8Array(testData.object);
 
-  expect(converted).toEqual(testData.byteArray);
+    expect(converted).toEqual(testData.byteArray);
+  });
 });
 
-/**
- * @target
- * uint8ArrayToObject should convert Uint8Array to object correctly
- *
- * @dependencies
- *
- * @scenario
- * - call the function under test
- * @expected
- * - converted value should be the expected one
- */
-it('should convert Uint8Array to object correctly', () => {
-  const converted = uint8ArrayToObject(testData.byteArray);
+describe('uint8ArrayToObject', () => {
+  /**
+   * @target
+   * uint8ArrayToObject should convert Uint8Array to object correctly
+   *
+   * @dependencies
+   *
+   * @scenario
+   * - call the function under test
+   * @expected
+   * - converted value should be the expected one
+   */
+  it('should convert Uint8Array to object correctly', () => {
+    const converted = uint8ArrayToObject(testData.byteArray);
 
-  expect(converted).toEqual(testData.object);
+    expect(converted).toEqual(testData.object);
+  });
 });
