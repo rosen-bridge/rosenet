8e61eb26ea7a4d1b6aa80c1cfa2c3d962f46f4e8
Mohammad Kermani <mkermani144@gmail.com>
2023-11-23 15:19:51 +0330

    doc(p2p-node): add missing test docs

--- packages/p2p-node/tests/lib/utils.spec.ts
+++ packages/p2p-node/tests/lib/utils.spec.ts
@@ -4,12 +4,34 @@ import { objectToUint8Array, uint8ArrayToObject } from '../../lib/utils';
 
 import { testData } from './utilsTestData';
 
+/**
+ * @target
+ * objectToUint8Array should convert object to Uint8Array correctly
+ *
+ * @dependencies
+ *
+ * @scenario
+ * - call the function under test
+ * @expected
+ * - converted value should be the expected one
+ */
 it('should convert object to Uint8Array correctly', () => {
   const converted = objectToUint8Array(testData.object);
 
   expect(converted).toEqual(testData.byteArray);
 });
 
+/**
+ * @target
+ * uint8ArrayToObject should convert Uint8Array to object correctly
+ *
+ * @dependencies
+ *
+ * @scenario
+ * - call the function under test
+ * @expected
+ * - converted value should be the expected one
+ */
 it('should convert Uint8Array to object correctly', () => {
   const converted = uint8ArrayToObject(testData.byteArray);
 
