1773c90f060435f29a1bd0e5c450a86cbe182082
Mohammad Kermani <mkermani144@gmail.com>
2024-02-14 14:34:36 +0000

    feat: log different libp2p node events for both node and relay

---
+++ .changeset/pink-pans-explode.md
@@ -0,0 +1,6 @@
+---
+'@rosen-bridge/rosenet-relay': minor
+'@rosen-bridge/rosenet-node': minor
+---
+
+log different libp2p node events
--- packages/rosenet-node/lib/createRoseNetNode.ts
+++ packages/rosenet-node/lib/createRoseNetNode.ts
@@ -5,13 +5,15 @@ import { mplex } from '@libp2p/mplex';
 import { tcp } from '@libp2p/tcp';
 import { createLibp2p } from 'libp2p';
 
+import { addEventListeners } from '@rosen-bridge/rosenet-utils';
+
 import RoseNetNodeError from './errors/RoseNetNodeError';
 
 import { RELAYS_COUNT_TO_CONNECT } from './constants';
 
 import { RoseNetNodeConfig } from './types';
 
-const createRoseNetNode = async (config: RoseNetNodeConfig) => {
+const createRoseNetNode = async ({ logger, ...config }: RoseNetNodeConfig) => {
   if (!config.relayMultiaddrs.length) {
     throw new RoseNetNodeError('Cannot start a RoseNet node without a relay');
   }
@@ -31,7 +33,9 @@ const createRoseNetNode = async (config: RoseNetNodeConfig) => {
       }),
     ],
   });
-  config.logger.debug('RoseNet node created');
+  logger.debug('RoseNet node created');
+
+  addEventListeners(node, logger);
 
   return {
     start: async () => node.start(),
