5103df0f713095f30ab3f63d8056089e7e2b3583
Mohammad Kermani <mkermani144@gmail.com>
2024-02-17 16:07:04 +0000

    test(simple-star): apply recent whitelist breaking changes
    In addition, extract relay private key into a local variable in
    terraform file.

--- tests/simple-star/src/node/node.ts
+++ tests/simple-star/src/node/node.ts
@@ -3,6 +3,7 @@ import { createRoseNetNode } from '@rosen-bridge/rosenet-node';
 const node = await createRoseNetNode({
   logger: console,
   relayMultiaddrs: [process.env.RELAY_MULTIADDR!],
+  privateKey: process.env.PRIVATE_KEY!,
 });
 
 node.start();
