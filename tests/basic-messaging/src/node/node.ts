94bd7e3cf64d5b4be2d3550fe631536a7a47aeeb
Mohammad Kermani <mkermani144@gmail.com>
2024-02-27 08:27:29 +0000

    test: add basic messaging test

---
+++ tests/basic-messaging/src/node/node.ts
@@ -0,0 +1,36 @@
+import { createRoseNetNode } from '@rosen-bridge/rosenet-node';
+
+const node = await createRoseNetNode({
+  logger: console,
+  relayMultiaddrs: [process.env.RELAY_MULTIADDR!],
+  privateKey: process.env.PRIVATE_KEY!,
+});
+
+await node.start();
+
+const messages = ['Hi', 'Ping', 'Bye'];
+
+if (process.env.MODE === 'dialer') {
+  setTimeout(async () => {
+    for (const message of messages) {
+      await node.sendMessage(process.env.LISTENER_PEER_ID!, message);
+    }
+    setTimeout(() => {
+      process.exit(0);
+    }, 2000);
+  }, 10000);
+} else {
+  node.handleIncomingMessage((from, message) => {
+    if (message === messages.at(-1)) {
+      setTimeout(() => {
+        process.exit(0);
+      }, 2000);
+    }
+  });
+}
+
+// This timeout works like a Promise.race, if above process.exit is not called
+// for some reason
+setTimeout(() => {
+  process.exit(0);
+}, 20000);
