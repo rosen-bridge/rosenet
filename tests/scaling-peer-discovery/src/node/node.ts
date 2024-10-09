2bf3fee5e43c7101b6013b41b6b037673b140734
Mohammad Kermani <mkermani144@gmail.com>
2024-10-09 13:34:32 +0330

    chore: remove redundant tests
    Remove all tests but one comprehensive one

--- tests/scaling-peer-discovery/src/node/node.ts
+++
@@ -1,9 +0,0 @@
-import { createRoseNetNode } from '@rosen-bridge/rosenet-node';
-
-const node = await createRoseNetNode({
-  logger: console,
-  relayMultiaddrs: [process.env.RELAY_MULTIADDR!],
-  privateKey: process.env.PRIVATE_KEY!,
-});
-
-await node.start();
