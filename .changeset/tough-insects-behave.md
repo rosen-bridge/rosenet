92c1f78f924c21c2797794361fb9de57805c2710
Mohammad Kermani <mkermani144@gmail.com>
2024-10-29 12:37:51 +0330

    fix(rosenet-node): fix failing dials when nodes are behind a non-symmetric NAT

---
+++ .changeset/tough-insects-behave.md
@@ -0,0 +1,5 @@
+---
+"@rosen-bridge/rosenet-node": patch
+---
+
+fix failing dials when nodes are behind a non-symmetric NAT
--- packages/rosenet-node/lib/stream/stream-service.ts
+++ packages/rosenet-node/lib/stream/stream-service.ts
@@ -1,4 +1,5 @@
 import { peerIdFromString } from '@libp2p/peer-id';
+import { Multiaddr } from '@multiformats/multiaddr';
 import {
   circuitBreaker,
   CircuitBreakerPolicy,
@@ -14,7 +15,10 @@ import { Libp2p } from 'libp2p';
 
 import RoseNetNodeContext from '../context/RoseNetNodeContext';
 
-import { ROSENET_DIRECT_PROTOCOL_V1 } from '../constants';
+import {
+  ROSENET_DIRECT_PROTOCOL_V1,
+  PUBLIC_MULTIADDR_DIAL_TIMEOUT,
+} from '../constants';
 
 import { RoseNetNodeError } from '../errors';
 
@@ -70,7 +74,50 @@ async function getRoseNetDirectStreamTo(to: string, node: Libp2p) {
    */
   const connection =
     possibleOpenConnectionToPeer ??
-    (await peerBreakers[to].execute(() => node.dial(peerId)));
+    (await peerBreakers[to].execute(async () => {
+      /**
+       * The nodes advertise hopefully public multiaddresses, but these
+       * multiaddresses may be wrong because of being behind a non-symmetric
+       * NAT. In addition, there is a bug in libp2p that allows the dial to a
+       * single multiaddress of a peer to take all the timeout of a dial to the
+       * peer. These combined will cause the dial to nodes with unreachable
+       * public multiaddresses to always fail, because their relayed
+       * multiaddress is never tried.
+       *
+       * As a workaround, we manually first try to dial the public multiaddress
+       * but with a reduced timeout, and then the other ones if the first dial
+       * fails.
+       *
+       * Libp2p bug GitHub issue:
+       * https://github.com/libp2p/js-libp2p/issues/2368
+       */
+      const peerAddresses = (await node.peerStore.get(peerId)).addresses;
+      const [publicAddresses, otherAddresses] = peerAddresses.reduce(
+        (partialPartitions, address) =>
+          address.multiaddr.isThinWaistAddress()
+            ? [
+                [...partialPartitions[0], address.multiaddr],
+                partialPartitions[1],
+              ]
+            : [
+                partialPartitions[0],
+                [...partialPartitions[1], address.multiaddr],
+              ],
+        [[], []] as [Multiaddr[], Multiaddr[]],
+      );
+
+      if (publicAddresses) {
+        try {
+          return await node.dial(publicAddresses, {
+            signal: AbortSignal.timeout(PUBLIC_MULTIADDR_DIAL_TIMEOUT),
+          });
+        } catch {
+          return await node.dial(otherAddresses);
+        }
+      } else {
+        return await node.dial(otherAddresses);
+      }
+    }));
 
   RoseNetNodeContext.logger.debug(
     possibleOpenConnectionToPeer
